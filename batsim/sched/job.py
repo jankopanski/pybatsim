af7ad6de9b19ebcc5bb93493db018cea4860fcd0
Steffen Lackner <lackner@cs.tu-darmstadt.de>
2017-08-17 16:52:19 +0200

    [sched] Add job and resource implementation

--- batsim/sched/job.py
+++ batsim/sched/job.py
@@ -1,4 +1,138 @@
+from batsim.batsim import Job as BatsimJob
 
 class Job:
 
-    pass
+    def __init__(self, batsim_job=None):
+        self._batsim_job = batsim_job
+        self._scheduled = False
+        self._submitted = False
+        self._rejected = False
+        self._rejected_reason = None
+        self._reservation = []
+        self._previous_reservations = []
+
+    def free_all(self):
+        self._previous_reservations.append(self._reservation[:])
+        for res in self._reservation[:]:
+            self.free(res)
+
+    def free(self, resource, recursive_call=False):
+        # To free resources the job does either have to be not submitted yet
+        # or the job has to be completed (i.e. the job status is set by batsim).
+        assert not self._submitted or self._batsim_job.status in ["SUCCESS", "TIMEOUT"]
+
+        if not recursive_call:
+            resource.free(self, recursive_call=True)
+        self._reservation.remove(resource)
+
+    def reserve(self, resource, recursive_call=False):
+        if not recursive_call:
+            resource.allocate(self, recursive_call=True)
+        self._reservation += resource.resources
+
+    @property
+    def scheduled(self):
+        return self._scheduled
+
+    @property
+    def rejected(self):
+        return self._rejected
+
+    @property
+    def reservation(self):
+        return tuple(self._reservation)
+
+    @property
+    def submitted(self):
+        return self._submitted
+
+    @property
+    def previous_reservations(self):
+        return tuple(self._previous_reservations)
+
+    def schedule(self, resource=None):
+        if resource:
+            self.reserve(resource)
+
+        assert not self.submitted
+        self._scheduled = True
+
+    def reject(self, reason=""):
+        assert not self.submitted and not self.scheduled
+        self._rejected = True
+        self._rejected_reason = reason
+
+    def _do_reject(self, scheduler):
+        scheduler.info("Rejecting job ({}), reason={}".format(self, self._rejected_reason))
+        scheduler._batsim.reject_jobs([self._batsim_job])
+        scheduler._open_jobs.remove(self)
+        scheduler._rejected_jobs.append(self)
+        del scheduler._job_map[self._batsim_job.id]
+
+    def submit(self, scheduler):
+        assert self._batsim_job is not None
+        assert not self.submitted and not self.rejected
+
+        if not self.scheduled:
+            self.schedule()
+
+        if self._batsim_job.requested_resources < len(self._reservation):
+            scheduler.warn("Starting of job ({}) is postponed since not enough resources are allocated"
+                    .format(self))
+            return
+
+        self._submitted = True
+
+        alloc = []
+        for res in self._reservation[:self._batsim_job.requested_resources]:
+            alloc.append(res.id)
+            res.computing = True
+
+        scheduler._batsim.start_jobs(
+                [self._batsim_job], {self._batsim_job.id: alloc})
+
+        self._scheduled = False
+
+        scheduler._open_jobs.remove(self)
+        scheduler._scheduled_jobs.append(self)
+
+        scheduler.info("Starting job ({})".format(self))
+
+    def __str__(self):
+        return str(self._batsim_job)
+
+    @property
+    def id(self):
+        return self._batsim_job.id
+
+    @property
+    def submit_time(self):
+        return self._batsim_job.submit_time
+
+    @property
+    def requested_time(self):
+        return self._batsim_job.requested_time
+
+    @property
+    def requested_resources(self):
+        return self._batsim_job.requested_resources
+
+    @property
+    def profile(self):
+        return self._batsim_job.profile
+
+    @property
+    def finish_time(self):
+        return self._batsim_job.finish_time
+
+    @property
+    def status(self):
+        return self._batsim_job.status
+
+    @property
+    def job_state(self):
+        return self._batsim_job.job_state
+
+    @property
+    def kill_reason(self):
+        return self._batsim_job.kill_reason
--- batsim/sched/resource.py
+++ batsim/sched/resource.py
@@ -1,4 +1,156 @@
 
 class Resource:
 
-    pass
+    def __init__(self, scheduler, batsim_id=-1):
+        self._scheduler = scheduler
+        self._id = batsim_id
+        self._allocated_by = []
+        self._previously_allocated_by = []
+        self._computing = False
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def is_allocated(self):
+        return bool(self._allocated_by)
+
+    @property
+    def allocated_by(self):
+        return tuple(self._allocated_by)
+
+    @property
+    def previously_allocated_by(self):
+        return tuple(self._previously_allocated_by)
+
+    def allocate(self, job, recursive_call=False):
+        assert not self.is_allocated, "Node sharing is currently not allowed"
+
+        if not recursive_call:
+            job.reserve(self, recursive_call=True)
+        self._allocated_by.append(job)
+
+    def free(self, job, recursive_call=False):
+        assert job in self._allocated_by, "Job is not allocated on this resource"
+
+        if not recursive_call:
+            job.free(self, recursive_call=True)
+
+        self._previously_allocated_by.append((self._scheduler.time, job))
+        self._allocated_by.remove(job)
+
+        if not self.is_allocated:
+            self.computing = False
+
+    @property
+    def computing(self):
+        return self._computing
+
+    @computing.setter
+    def computing(self, value):
+        self._computing = value
+
+    @property
+    def resources(self):
+        return [self]
+
+
+class Resources:
+
+    def __init__(self, from_list=[]):
+        self._resources = list(from_list)
+
+    def allocate(self, job, recursive_call=False):
+        for r in self._resources:
+            r.allocate(job, recursive_call=recursive_call)
+
+    def free(self, job, recursive_call=False):
+        for r in self._resources:
+            r.free(job, recursive_call=recursive_call)
+
+    @property
+    def resources(self):
+        return tuple(self._resources)
+
+    def __add__(self, other):
+        return Resources(set(self._resources + other._resources))
+
+    def filter(self, cond=None, sort=None, free=False, allocated=False, limit=None, min=None, num=None, for_job=None):
+        nr = []
+
+        # Pre-defined filter to find resources for a job submission
+        if for_job is not None:
+            free = True
+            allocated = False
+            num = for_job.requested_resources
+
+        # Yield all resources if not filtered
+        if not free and not allocated:
+            free = True
+            allocated = True
+
+        # If a concrete number of resources is requested do not yield less or more
+        if num:
+            min = num
+            limit = num
+
+        # Filter free or allocated resources
+        for r in self._resources:
+            if r.is_allocated:
+                if allocated:
+                    nr.append(r)
+            else:
+                if free:
+                    nr.append(r)
+
+        # Filter applying a given condition
+        if cond:
+            nr2 = nr
+            nr = []
+            for r in nr2:
+                if cond(r):
+                    nr.append(r)
+
+        # Sort the resources if a function was given. Resources which should be
+        # preferred to be chosen can be sorted to the front of the list.
+        if sort:
+            nr = sort(nr)
+
+        # Do not yield more resources than requested
+        if limit:
+            nr = nr[:limit]
+
+        # Do not yield less resources than requested (better nothing than less)
+        if min and len(nr) < min:
+            nr = []
+
+        # Construct a new resources list which can be filtered again
+        return Resources(nr)
+
+    def __len__(self):
+        return len(self._resources)
+
+    def __getitem__(self, items):
+        return self._resources[items]
+
+    def __delitem__(self, index):
+        del self._resources[index]
+
+    def __setitem__(self, index, element):
+        self._resources[index] = element
+
+    def __str__(self):
+        return str(self._resources)
+
+    def append(self, element):
+        self._resources.append(element)
+
+    def remove(self, element):
+        self._resources.remove(element)
+
+    def insert(self, index, element):
+        self._resources.insert(index, element)
+
+    def __iter__(self):
+        return iter(self._resources)
