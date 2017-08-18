b470c4e5d2f9d5f80eb8e07408fe8772327fe19c
Steffen Lackner <lackner@cs.tu-darmstadt.de>
2017-08-18 13:25:30 +0200

    [sched] Add abstraction for profiles (for dynamic jobs)

--- batsim/sched/__init__.py
+++ batsim/sched/__init__.py
@@ -3,8 +3,11 @@ from .scheduler import *
 from .job import *
 from .reply import *
 from .resource import *
+from .profiles import *
 
 __all__ = [
     Scheduler,
-    Job
+    Job,
+    Profile,
+    Profiles
 ]
---
+++ batsim/sched/profiles.py
@@ -0,0 +1,25 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Profile(metaclass=ABCMeta):
+
+    @abstractmethod
+    def to_dict(self):
+        pass
+
+    def __call__(self):
+        return self.to_dict()
+
+
+class Profiles(metaclass=ABCMeta):
+
+    class Delay(Profile):
+
+        def __init__(self, time):
+            self.time = time
+
+        def to_dict(self):
+            return {
+                    "type": "delay",
+                    "delay": self.time
+                   }
