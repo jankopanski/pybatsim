e0b3e2f66c50c7d554c617790d62f464a6c0b50c
Steffen Lackner <lackner@cs.tu-darmstadt.de>
2017-09-04 10:25:42 +0200

    [sched] autopep8

--- batsim/sched/alloc.py
+++ batsim/sched/alloc.py
@@ -238,9 +238,10 @@ class Allocation:
                 res = self._resources[i]
                 if res in self._allocated_resources:
                     self._allocated_resources.clear()
-                    scheduler.fatal("Resource ranges in allocation are invalid: {res}",
-                            res=res,
-                            type="resource_ranges_invalid")
+                    scheduler.fatal(
+                        "Resource ranges in allocation are invalid: {res}",
+                        res=res,
+                        type="resource_ranges_invalid")
                 self._allocated_resources.add(res)
                 res._do_allocate_allocation(self)
         self._allocated = True
