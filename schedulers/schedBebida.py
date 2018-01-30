ecac93a0433420f61f2b1195e5b283f48f39b27c
MERCIER Michael <michael.mercier@inria.fr>
2018-01-30 16:50:22 +0100

    Move to lower abstraction API to do scheduling for big data

--- schedulers/schedBebida.py
+++ schedulers/schedBebida.py
@@ -12,43 +12,78 @@
 
 """
 
-from batsim.sched import Scheduler, Jobs
-from batsim.sched.algorithms.filling import filler_sched
-from batsim.sched.algorithms.utils import default_resources_filter
+from batsim.batsim import BatsimScheduler
 
 from procset import ProcSet
 import itertools
 
 
-def to_set(objects):
-    return [o.id for o in objects]
+class SchedBebida(BatsimScheduler):
 
+    def filter_jobs_by_state(self, state):
+        return [job for job in self.jobs if job.job_state == state]
 
-class SchedBebida(Scheduler):
+    def runnning_jobs(self):
+        return filter_jobs_by_state(Job.state.RUNNING)
+
+    def submitted_jobs(self):
+        return filter_jobs_by_state(Job.state.SUBMITTED)
+
+    def allocate_first_fit_in_best_effort(job):
+        """
+        return the allocation with as much resources as possible up to
+        the job's `requeqted_resources` number.
+        return None if no resources at all are available.
+        """
+        logger.info("Try to allocate Job: {}".format(job.id));
+        nb_found_resources = 0
+        allocation = ProcSet()
+        nb_resources_still_needed = job.requested_resources
+
+        curr_interval = next(self.free_resources.intervals)
+
+        while (len(allocation) < job.requested_resources or curr_interval is None):
+            interval_size = len(interval);
+
+            if interval_size > nb_resources_still_needed:
+                allocation = allocation + ProcInt(
+                        inf=curr_interval.inf,
+                        sup=(curr_interval + nb_resources_still_needed -1))
+            elif interval_size == nb_resources_still_needed:
+                allocation.add(ProcInt(*curr_interval))
+            elif interval_size < nb_resources_still_needed:
+                allocation.add(ProcInt(*curr_interval))
+                nb_resources_still_needed = nb_resources_still_needed - interval_size
+                curr_interval = next(self.free_resources.intervals)
+        job.allocation = allocation
+        logger.info("Allocation for job {}: {}".format(job.id, job.allocation));
+
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.to_be_removed_resources = {}
 
-    def on_remove_resources(self, resources):
+    def onSimulationBegins(self):
+        self.free_resources = ProcSet(*[res["id"] for res in self.resources])
+
+
+    def onRemoveResources(self, resources):
         # find the list of jobs that are impacted
         # and kill all those jobs
         #import ipdb; ipdb.set_trace()
         self.to_be_removed_resources[resources] = []
-        to_be_killed = Jobs()
-        for job in self.jobs.running:
-            if ProcSet(*to_set(job.allocation)) & ProcSet.from_str(resources):
+        to_be_killed = []
+        for job in self.running_jobs():
+            if job.allocation & ProcSet.from_str(resources):
                 to_be_killed.add(job)
         self._batsim.kill_jobs(to_be_killed)
         self.to_be_removed_resources[resources] = to_be_killed
 
-    def on_add_resources(self, resources):
+    def onAddResources(self, resources):
         # add the resources
         for resource in ProcSet.from_str(resources):
-            bat_res = { id: resource }
-            self._batsim.resources.append(bat_res)
-        # Initialize new API data structure
-        self._on_pre_init()
+            #import ipdb; ipdb.set_trace()
+            self.resources[resource].unavailable = False
 
         # find the list of jobs that need more resources
         # kill jobs, so tey will be resubmited taking free resources, until
@@ -64,7 +99,7 @@ class SchedBebida(Scheduler):
                 break
         self._batsim.kill_jobs(to_be_killed)
 
-    def on_jobs_killed(self, jobs):
+    def onJobsKilled(self, jobs):
         # check if all jobs associated to one decomission are killed
         #for job in jobs:
         #    for _, to_be_killed in self.to_be_removed_resources.items():
@@ -95,7 +130,7 @@ class SchedBebida(Scheduler):
         #     job.get_job_data("progress")
 
     def schedule(self):
-        return filler_sched(
-            self,
-            resources_filter=default_resources_filter,
-            abort_on_first_nonfitting=True)
+        # Implement a simple FIFO scheduler
+        print("Do schedule")
+        for job in self.submitted_jobs():
+
