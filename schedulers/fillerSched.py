8cf74cafce3f6808a1f627074c125058c0974651
clement-dell <clement.mommessin@inria.fr>
2018-08-31 16:07:24 +0200

    Removed start_jobs_* and updated schedulers to use procset

--- schedulers/fillerSched.py
+++ schedulers/fillerSched.py
@@ -2,8 +2,8 @@ from batsim.batsim import BatsimScheduler, Batsim
 
 import sys
 import os
-from random import sample
-from sortedcontainers import SortedSet
+from procset import ProcSet, ProcInt
+from itertools import islice
 
 
 class FillerSched(BatsimScheduler):
@@ -14,15 +14,13 @@ class FillerSched(BatsimScheduler):
     def onAfterBatsimInit(self):
         self.nb_completed_jobs = 0
 
-        self.jobs_res = {}
         self.jobs_completed = []
         self.jobs_waiting = []
 
         self.sched_delay = 0.005
 
         self.openJobs = set()
-        self.availableResources = SortedSet(range(self.bs.nb_resources))
-        self.previousAllocations = dict()
+        self.availableResources = ProcSet(ProcInt(0,self.bs.nb_compute_resources-1))
 
 
     def scheduleJobs(self):
@@ -30,20 +28,17 @@ class FillerSched(BatsimScheduler):
 
         print('openJobs = ', self.openJobs)
         print('available = ', self.availableResources)
-        print('previous = ', self.previousAllocations)
 
         # Iterating over a copy to be able to remove jobs from openJobs at traversal
         for job in set(self.openJobs):
             nb_res_req = job.requested_resources
 
             if nb_res_req <= len(self.availableResources):
-                res = self.availableResources[:nb_res_req]
-                self.jobs_res[job.id] = res
-                self.previousAllocations[job.id] = res
+                job_alloc = ProcSet(*islice(self.availableResources, nb_res_req))
+                job.allocation = job_alloc
                 scheduledJobs.append(job)
 
-                for r in res:
-                    self.availableResources.remove(r)
+                self.availableResources -= job_alloc
 
                 self.openJobs.remove(job)
 
@@ -52,11 +47,10 @@ class FillerSched(BatsimScheduler):
 
         # send to uds
         if len(scheduledJobs) > 0:
-            self.bs.start_jobs(scheduledJobs, self.jobs_res)
+            self.bs.execute_jobs(scheduledJobs)
 
         print('openJobs = ', self.openJobs)
         print('available = ', self.availableResources)
-        print('previous = ', self.previousAllocations)
         print('')
 
     def onJobSubmission(self, job):
@@ -64,7 +58,5 @@ class FillerSched(BatsimScheduler):
         self.scheduleJobs()
 
     def onJobCompletion(self, job):
-        for res in self.previousAllocations[job.id]:
-            self.availableResources.add(res)
-        self.previousAllocations.pop(job.id)
+        self.availableResources |= job.allocation
         self.scheduleJobs()
