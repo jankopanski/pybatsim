2f0a3a88bcce01c0507011283801456c5d7d5937
Mommessc <clement.mommessin@inria.fr>
2020-01-22 08:04:45 +0100

    Update replication-based policies

--- schedulers/greco/qarnotNodeSchedReplicate10LeastLoaded.py
+++ schedulers/greco/qarnotNodeSchedReplicate10LeastLoaded.py
@@ -22,40 +22,11 @@ class QarnotNodeSchedReplicate10LeastLoaded(QarnotNodeSched):
     def __init__(self, options):
         super().__init__(options)
 
-        if "output_path" in options:
-            self.output_filename = options["output_path"] + "/out_pybatsim.csv"
-        else:
-            self.output_filename = None
-
         self.nb_least_loaded = 10
 
 
-
-    def onJobSubmission(self, job, resubmit=False):
-        qtask_id = job.id.split('_')[0]
-        job.qtask_id = qtask_id
-
-        # Retrieve or create the corresponding QTask
-        if not qtask_id in self.qtasks_queue:
-            assert resubmit == False, "QTask id not found during resubmission of an instance"
-
-            list_datasets = self.bs.profiles[job.workload][job.profile]["datasets"]
-            if list_datasets is None:
-                list_datasets = []
-            user_id = job.profile_dict["user"]
-
-            qtask = QTask(qtask_id, job.profile_dict["priority"], list_datasets, user_id)
-            self.qtasks_queue[qtask_id] = qtask
-            self.nb_received_qtasks += 1
-            # Ask for the replication of its datasets
-            storage_ids = self.storage_controller.getNMostEmptyStorages(self.nb_least_loaded)
-            for dataset_id in qtask.datasets:
-                self.storage_controller.replicateDatasetOnStorages(dataset_id, storage_ids)
-        else:
-            qtask = self.qtasks_queue[qtask_id]
-        self.nb_received_instances += 1
-
-        qtask.instance_submitted(job, resubmit)
-
-        self.do_dispatch = True
-        #TODO maybe we'll need to disable this dispatch, same reason as when a job completes
+    def doDatasetReplication(self, qtask):
+        # Replicate on 3 least loaded disks
+        storage_ids = self.storage_controller.getNMostEmptyStorages(self.nb_least_loaded)
+        for dataset_id in qtask.datasets:
+            self.storage_controller.replicateDatasetOnStorages(dataset_id, storage_ids)
